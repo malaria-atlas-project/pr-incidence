fa423506c7b0084adc19e0141c52a61cedfebb0e
PWG <peter.gething@zoo.ox.ac.uk>
2009-05-22 17:56:58 +0100

    changed dimension checks

--- pr_incidence/predictive_fn.py
+++ pr_incidence/predictive_fn.py
@@ -39,8 +39,10 @@ class BurdenPredictor(object):
         """
         Expects a pr array. Should be of same shape as the pop array that was received as input.
         """
-        if pr.shape != (0,pop[::pop_pr_res].shape[1]):
+        #if pr.shape != (0,pop[::pop_pr_res].shape[1]):
+        pr.shape[0]!=np.shape(pop[:,::pop_pr_res])[1]:
             raise ValueError, 'PR input has shape %s, but the population input had shape %s.'%(pr.shape, pop.shape)
+        #if len(pr.shape)>0:
         if len(pr.shape)>0:
             raise ValueError, 'PR is supposed to be 1d, dumbass.'
 
